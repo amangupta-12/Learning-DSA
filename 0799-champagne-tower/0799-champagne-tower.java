a0acf5384af661feddcbb2c956a8825a27ab5cf4
Aman Gupta <amg8424934129@gmail.com>
2026-09-07 18:07:31 +0530

    Time: 16 ms (3.87%), Space: 47.1 MB (58.43%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.java
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -1,25 +1,21 @@
 class Solution {
 
-    private double solve(int i ,int j , int poured,double[][] dp){
-        if(i==0 && j==0) return poured;
-
-        if(i<0 || j<0 || i<j) return 0.0;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        double left = (solve(i-1,j,poured,dp)-1)/2.0;
-        double right  = (solve(i-1,j-1,poured,dp)-1)/2.0;
-        if(left < 0) left = 0.0;
-        if(right < 0) right = 0.0;
-        return dp[i][j] = left + right;
-    }
-    public double champagneTower(int poured, int query_row, int query_glass) {
-        if(query_row == 0 && query_glass == 0) return Math.min(1.0,poured);
+    
+    public double champagneTower(int poured, int r, int c) {
+        if(r== 0 && c == 0) return Math.min(1.0,poured);
         double[][] dp = new double[101][101];
-       for(int i=0;i<101;i++){
+        dp[0][0] = poured;
+
+      for(int i=1;i<101;i++){
         for(int j=0;j<101;j++){
-            dp[i][j] = -1;
+            double left =  (i<j)  ? 0.0 : Math.max(0.0,(dp[i-1][j]-1)/2.0);
+            double right = (i<j || j-1<0) ? 0.0 : Math.max(0.0,(dp[i-1][j-1]-1)/2.0);
+
+          
+            dp[i][j] = left + right;
         }
-       }
-        return Math.min(1.0,solve( query_row, query_glass,poured,dp));
+      }
+
+       return Math.min(1.0,dp[r][c]);
     }
 }
