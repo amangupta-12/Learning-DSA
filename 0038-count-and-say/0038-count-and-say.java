cde60f6f02f7df193516ee655f5f3c9f7fb11105
Aman Gupta <amg8424934129@gmail.com>
2026-09-25 16:36:59 +0530

    Time: 9 ms (33.84%), Space: 43.3 MB (28.5%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,10 +1,34 @@
 class Solution {
-    String solve(int n){
-        if(n == 1){
-            return "1";
-        }
-        String prev = solve(n-1);
-        int count = 1;
+//     String solve(int n){
+//         if(n == 1){
+//             return "1";
+//         }
+//         String prev = solve(n-1);
+//         int count = 1;
+//         StringBuilder sb = new StringBuilder();
+//         for(int i=1;i<prev.length();i++){
+//             if(prev.charAt(i) == prev.charAt(i-1)){
+//                 count++;
+//             }else{
+//                 sb.append(count);
+//                 sb.append(prev.charAt(i-1));
+//                 count = 1;
+//             }
+//         }
+//         sb.append(count);
+//         sb.append(prev.charAt(prev.length()-1));
+
+//         return sb.toString();
+//     }
+//     public String countAndSay(int n) {
+//         return solve(n);
+//     }
+// }
+
+public String countAndSay(int n) {
+    String prev = "1";
+    for(int j=0;j<n;j++){
+         int count = 1;
         StringBuilder sb = new StringBuilder();
         for(int i=1;i<prev.length();i++){
             if(prev.charAt(i) == prev.charAt(i-1)){
@@ -17,10 +41,11 @@ class Solution {
         }
         sb.append(count);
         sb.append(prev.charAt(prev.length()-1));
+         prev = (j == n-1) ? prev : sb.toString();
+    }
 
-        return sb.toString();
-    }
-    public String countAndSay(int n) {
-        return solve(n);
-    }
+    return prev;
 }
+}
+
+       
