2ff2ce531514e7d19c6c6aac6e15b40f6e15e321
Aman Gupta <amg8424934129@gmail.com>
2026-09-25 16:27:43 +0530

    Time: 2 ms (99.7%), Space: 42.7 MB (96.18%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,26 @@
+class Solution {
+    String solve(int n){
+        if(n == 1){
+            return "1";
+        }
+        String prev = solve(n-1);
+        int count = 1;
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<prev.length();i++){
+            if(prev.charAt(i) == prev.charAt(i-1)){
+                count++;
+            }else{
+                sb.append(count);
+                sb.append(prev.charAt(i-1));
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(prev.charAt(prev.length()-1));
+
+        return sb.toString();
+    }
+    public String countAndSay(int n) {
+        return solve(n);
+    }
+}
